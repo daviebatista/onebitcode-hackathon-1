5dc37335217e6133ce2576efbad83a6112dd0343
Davi Batista <davideosmar11@gmail.com>
2023-07-04 09:38:06 -0300

    feat: added project structure and its components and subcomponents

---
+++ src/components/Gallery/GallerySlide.tsx
@@ -0,0 +1,19 @@
+import '@splidejs/react-splide/css';
+//@ts-ignore
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+function Gallery()  {
+    return(
+        <>
+            <Splide>
+                <SplideSlide>...</SplideSlide>
+                <SplideSlide>...</SplideSlide>
+                <SplideSlide>...</SplideSlide>
+                <SplideSlide>...</SplideSlide>
+                <SplideSlide>...</SplideSlide>
+            </Splide>
+        </>
+    )
+}
+
+export default Gallery
