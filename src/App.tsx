4693638a8d7be18446cb704dfbac8c96aabff7bb
Davi Batista <davideosmar11@gmail.com>
2023-07-04 19:21:47 -0300

    feat: added options to 'about' section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About/About"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header/>
       <Hero/>
+      <About/>
     </>
   )
 }
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,8 +2,19 @@ import styles from "./styles.module.scss";
 
 function About ()  {
     return  (
-                <>
-                </>
+                <section className={styles.section}>
+                    <div className={styles.pictures}>
+                        <img src="./src/assets/about-us-photography-1.svg" alt="about-us-photography-1" className={styles.image + " over"}/>
+                    </div>
+                    <div className={styles.description}>
+                        <h1>Sobre a gente</h1>
+                        <p>Na onebitmusic, estamos redefinindo a maneira como experimentamos e nos envolvemos com a música, utilizando tecnologia de realidade virtual (VR) para criar shows e performances que transcendem os limites físicos e levam <br/> você a novos patamares de entretenimento musical.</p>
+                        <a href="" className={styles.button}>
+                            <p>Ler mais</p>
+                            <img src="./src/assets/images/resources/arrow.svg" alt="" />
+                        </a>
+                    </div>
+                </section>
     )
 }
 
