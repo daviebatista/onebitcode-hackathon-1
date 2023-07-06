3bb2aad4e551f3c7d535457ac955429c7699bc47
Davi Batista <davideosmar11@gmail.com>
2023-07-06 12:46:09 -0300

    feat: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About/About"
+import Footer from "./components/Footer/Footer"
 import Gallery from "./components/Gallery/Gallery"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
@@ -18,6 +19,7 @@ function App() {
       <Schedule/>
       <Sponsors/>
       <Gallery/>
+      <Footer/>
     </>
   )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,22 @@ import styles from "./styles.module.scss";
 
 function Footer ()  {
     return  (
-                <>
-                </>
+                <section className={styles.footer}>
+                    <div className={styles.logo}>
+                        <span className={styles.onebit}>onebit</span>
+                        <span className={styles.music}>music</span>
+                        <img src="../../../src/assets/images/resources/onebit-logo.svg" alt="" className={styles.logoOneBit + " px-1"}/>
+                    </div>
+                    <div className={styles.rightSide}>
+                        <span className={styles.iconsDiv}>
+                            <img src="./src/assets/images/icons/facebook.svg" alt="facebookLogo" className={styles.icons}  style={{width: "10%"}}/>
+                            <img src="./src/assets/images/icons/twitter.svg" alt="twitterLogo" className={styles.icons} />
+                            <img src="./src/assets/images/icons/linkedin.svg" alt="linkedInLogo" className={styles.icons} />
+                            <img src="./src/assets/images/icons/instagram.svg" alt="instagramLogo" className={styles.icons} />
+                        </span>
+                        <p className={styles.brand}>© onebitcode.com, 2023 </p>
+                    </div>
+                </section>
     )
 }
 
